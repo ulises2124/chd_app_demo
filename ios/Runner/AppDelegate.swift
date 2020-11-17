192f91b27f4c3fd361da27e339ad643f439ade8a
ulises <ulises.gonzalez@axity.com>
2020-11-17 17:58:06 -0600

    se agrega api keys de los mapas

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,17 @@
 import UIKit
 import Flutter
-
+import GoogleMaps
+//#import "GoogleMaps/GoogleMaps.h"
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+   // GMSServices provideAPIKey "";
+  // [GMSServices provideAPIKey:@"AIzaSyAe2bHPLoGQqkqizRjtvsyFF2bfEuLd6Xg"];
+    GMSServices.provideAPIKey("AIzaSyAe2bHPLoGQqkqizRjtvsyFF2bfEuLd6Xg")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
